9e3a709cabc9b265a0ba366742c818bb649f5640
연유 <yeonyew@gmail.com>
2024-08-19 11:20:27 +0900

    fix: resolve index ejs base

--- packages/root/vite.config.ts
+++ packages/root/vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
   plugins: [
     ViteEjsPlugin((config) => {
       return {
-        root: config.root,
+        BASE: config.base,
       };
     }),
     viteStaticCopy({
